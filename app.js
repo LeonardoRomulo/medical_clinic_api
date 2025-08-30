c48c0d48d67720ee180d424babe249fa0b58985b
LeonardoRomulo <leo.romulo15@gmail.com>
2025-08-30 15:21:31 -0300

    feat: configuração do app.js com rotas e middlewares

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import medicosRouters from './Routers/medicosRouters.js';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(medicosRouters);
+
+app.listen( 9000, () => {
+    const date = new Date ();
+    console.log(`Servidor iniciado em: ${date}`);
+})
