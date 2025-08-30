124ba03257517f6527b1feeb02e8854e81990e75
LeonardoRomulo <leo.romulo15@gmail.com>
2025-08-30 12:21:16 -0300

    feat: Configuração da conexão do banco de dados e a criação da classe construtora médicos

---
+++ model/medicos.js
@@ -0,0 +1,10 @@
+class medicos {
+    constructor(nome, telefone, email,descricao){
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.descricao = descricao;
+    }
+}
+
+export default medicos;
